4e3a105b2b81ceef598e9904d80406e4cfbdefe8
Mehdi Salehi <ezzobad@gmail.com>
2023-08-17 12:15:04 +1000

    Delete mycdktest directory

--- mycdktest/bin/mycdktest.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { MycdktestStack } from '../lib/mycdktest-stack';
-
-const app = new cdk.App();
-new MycdktestStack(app, 'MycdktestStack');
