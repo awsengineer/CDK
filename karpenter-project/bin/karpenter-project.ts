214bf96808ab10df2dd5101677e04d7e7fe4dcfd
Mehdi Salehi <ezzobad@gmail.com>
2023-08-17 12:15:25 +1000

    Delete karpenter-project directory

--- karpenter-project/bin/karpenter-project.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { KarpenterProjectStack } from '../lib/karpenter-project-stack';
-
-const app = new cdk.App();
-new KarpenterProjectStack(app, 'KarpenterProjectStack');
